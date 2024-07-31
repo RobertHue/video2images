a4f1fb81e50e1ac0e5245f61457ecaa99c4e2716
Robert Hümmer <robert.huem@gmail.com>
2024-07-31 13:01:44 +0200

    improved video2images logging and others

--- video2images.py
+++ video2images.py
@@ -8,17 +8,22 @@ from pathlib import Path
 # 3rd party
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Constants
-GOOD_MATCH_DISTANCE_THRESHOLD = 30  # Threshold for determining a good match
+GOOD_MATCH_DISTANCE_THRESHOLD = 200  # Threshold for determining a good match
 
-# Set up logging
+# Basic configuration for the root logger
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    level=logging.DEBUG,  # Set default level for root logger
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('logging.log'),  # File handler
+        logging.StreamHandler()  # Console handler
+    ]
 )
 
-
-def is_image_sharp(image, threshold=100.0):
+def is_image_sharp(image, threshold=50.0):
     """
     Check if the image is in focus based on the variance of the Laplacian.
 
@@ -47,7 +52,36 @@ def is_image_well_exposed(image, low_threshold=0.1, high_threshold=0.9):
     return (cdf[0] > low_threshold) and (cdf[-1] < high_threshold)
 
 
-def is_overlapping(image1, image2, overlap_fraction=0.6):
+def debug_matches(gray1, gray2, kp1, kp2, matches, good_matches):
+    logging.info(f"{len(good_matches)=}")
+    logging.info(f"{len(kp1)=}")
+    # Sort them in the order of their distance.
+    matches = sorted(matches, key = lambda x:x.distance)
+
+    # Draw first 10 matches.
+    img_with_keypoints = cv2.drawKeypoints(gray1, kp1, None, color=(0, 255, 0), flags=cv2.DrawMatchesFlags_DRAW_RICH_KEYPOINTS)
+    # Convert BGR image to RGB (matplotlib uses RGB)
+    img_with_keypoints_rgb = cv2.cvtColor(img_with_keypoints, cv2.COLOR_BGR2RGB)
+
+    img4 = cv2.drawMatches(gray1,kp1,gray2,kp2,good_matches,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    plt.title("Are frames overlapping by at least 60%")
+
+    # Display the images
+    ax1.imshow(img_with_keypoints_rgb)
+    ax1.set_title('Image 3')
+
+    ax2.imshow(img4)
+    ax2.set_title('Image 4')
+
+    plt.figtext(0.5, 0.01, f"{len(good_matches)=} {len(matches)=} {len(kp1)=}", ha='center', fontsize=12)
+
+    # Show the plot
+    plt.show()
+
+
+def is_overlapping(image1, image2, overlap_fraction=0.4):
     """
     Check if the overlap percentage between two images using ORB feature matching.
 
@@ -55,18 +89,30 @@ def is_overlapping(image1, image2, overlap_fraction=0.6):
     :param image2: Second input image in BGR format.
     :param overlap_fraction: Fraction of overlap required between images.
     :return: True if the overlap is greater than or equal to the specified fraction.
+             False if one of the images lack details or distinct features...
     """
     gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
     gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
     orb = cv2.ORB_create()
     kp1, des1 = orb.detectAndCompute(gray1, None)
-    _, des2 = orb.detectAndCompute(gray2, None)
+    kp2, des2 = orb.detectAndCompute(gray2, None)
+
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    if des1 is None or des2 is None:
+        logging.error("One of the images lack details or distinct features!")
+        return False
+
     matches = bf.match(des1, des2)
+
     good_matches = [m for m in matches if m.distance < GOOD_MATCH_DISTANCE_THRESHOLD]
-    if len(kp1) == 0:
+    if len(kp1) == 0: # avoids division by 0
         return False
     overlap_percentage = len(good_matches) / len(kp1)
+
+    # if  overlap_percentage < overlap_fraction:
+    #     debug_matches(gray1, gray2, kp1, kp2, matches, good_matches)
+
     return overlap_percentage >= overlap_fraction
 
 
@@ -89,12 +135,11 @@ def clear_output_directory(directory_path):
         )
 
 
-def extract_frames(video_path, overlap_fraction):
+def extract_frames(video_path):
     """
     Extract frames from a video file and perform quality checks on them.
 
     :param video_path: Path to the video file.
-    :param overlap_fraction: Minimum overlap fraction between frames.
     """
     path_obj = Path(video_path)
     directory_path = path_obj.parent
@@ -126,12 +171,12 @@ def extract_frames(video_path, overlap_fraction):
             count += 1
 
             # Quality checks
-            # if not is_image_sharp(frame):
-            #     logging.warning(f"Frame {count} is not sharp; hence too blurry. Skipping...")
-            #     continue
+            if not is_image_sharp(frame):
+                logging.warning(f"Frame {count} is not sharp; hence too blurry. Skipping...")
+                continue
 
-            if prev_frame is not None and not is_overlapping(prev_frame, frame, overlap_fraction):
-                logging.warning(f"Frame {count} does not overlap with previous frame by at least {overlap_fraction*100}%. Skipping...")
+            if prev_frame is not None and not is_overlapping(prev_frame, frame):
+                logging.warning(f"Frame {count} does not overlap with previous frame enough. Skipping...")
                 continue
 
             # Save frame as image
@@ -153,10 +198,9 @@ def main():
     """
     parser = argparse.ArgumentParser(description="Split video into images")
     parser.add_argument("video_path", type=str, help="Path to the video file")
-    parser.add_argument("--overlap_fraction", type=float, default=0.6, help="Minimum overlap fraction between frames")
     args = parser.parse_args()
 
-    extract_frames(args.video_path, args.overlap_fraction)
+    extract_frames(args.video_path)
 
 
 if __name__ == "__main__":
