ae0293d733dbb472d62496d60398eac0a89105cf
Robert Hümmer <robert.huem@gmail.com>
2024-08-02 12:26:24 +0200

    moved

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,4 +1,3 @@
-from .debug import debug_matches
 from .features import get_feature_match_ratio
 from .features import get_laplacian_variance
 from .features import is_well_exposed
@@ -8,7 +7,6 @@ from .geometric_overlap import compute_overlap
 
 # Expose the most commonly used components in the package's namespace
 __all__ = [
-    "debug_matches",
     "get_laplacian_variance",
     "is_well_exposed",
     "get_feature_match_ratio",
